7497f31bd97e7a98d5a75a6189ba0c916cc1a5e5
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-25 17:17:07 -0600

    Add answers with an array as a key value pair

--- data/questions.js
+++ data/questions.js
@@ -1,33 +1,44 @@
 export const questions = [
   {
     "id": 1,
-    "questions": "Explain what ARIA and screenreaders are, and how to make a website accessible."
+    "questions": "Explain what ARIA and screenreaders are, and how to make a website accessible.",
+    "answers": [],
   }, {
     "id": 2,
-    "questions": "What is the virtual DOM in React?"
+    "questions": "What is the virtual DOM in React?",
+    "answers": [],
+    
   }, {
     "id": 3,
-    "questions": "How are props different from state in React?"
+    "questions": "How are props different from state in React?",
+    "answers": [],
   }, {
     "id": 4,
-    "questions": "Explain 'hoisting'."
+    "questions": "Explain 'hoisting'.",
+    "answers": [],
   }, {
     "id": 5,
-    "questions": "Describe event bubbling."
+    "questions": "Describe event bubbling.",
+    "answers": [],
   }, {
     "id": 6,
-    "questions": "What are the differences between variables created using let, var or const?"
+    "questions": "What are the differences between variables created using let, var or const?",
+    "answers": [],
   }, {
     "id": 7,
-    "questions": "What is the definition of a higher-order function?"
+    "questions": "What is the definition of a higher-order function?",
+    "answers": [],
   }, {
     "id": 8,
-    "questions": "Explain how prototypal inheritance works."
+    "questions": "Explain how prototypal inheritance works.",
+    "answers": [],
   }, {
     "id": 9,
-    "questions": "What's the difference between a variable that is: null, undefined or undeclared?"
+    "questions": "What's the difference between a variable that is: null, undefined or undeclared?",
+    "answers": [],
   }, {
     "id": 10,
-    "questions": "What is a promise? Where and how would you use promise?"
+    "questions": "What is a promise? Where and how would you use promise?",
+    "answers": [],
   }
 ]
