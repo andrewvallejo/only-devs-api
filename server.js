19680c1fe66b4aad5877ede8d09421bb6f7ee45e
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-25 16:06:57 -0600

    Move cors to come before other use methods

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ import cors from 'cors';
 import { questions } from './data/questions';
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(express.static('public'));
-app.use(cors())
 app.set('port', process.env.PORT || 3000);
 app.locals = { 
   title: 'onlyDevs API',
