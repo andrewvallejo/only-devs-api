d2e498e38ca965d2c6239e4bf80639054d889cdc
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-25 15:51:20 -0600

    Add server skeleton with imports

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import { questions } from './data/questions';
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use(cors())
+app.set('port', process.env.PORT || 3000);
+app.locals = { 
+  title: 'onlyDevs API',
+  questions
+}
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+});
