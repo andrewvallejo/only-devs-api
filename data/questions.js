82bb35c6606fcfacc835e5738fe7afaf9454fad3
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-25 16:34:15 -0600

    Add ten question stubs

---
+++ data/questions.js
@@ -0,0 +1,33 @@
+export const questions = [
+  {
+    "id": 1,
+    "questions": "Explain what ARIA and screenreaders are, and how to make a website accessible."
+  }, {
+    "id": 2,
+    "questions": "What is the virtual DOM in React?"
+  }, {
+    "id": 3,
+    "questions": "How are props different from state in React?"
+  }, {
+    "id": 4,
+    "questions": "Explain 'hoisting'."
+  }, {
+    "id": 5,
+    "questions": "Describe event bubbling."
+  }, {
+    "id": 6,
+    "questions": "What are the differences between variables created using let, var or const?"
+  }, {
+    "id": 7,
+    "questions": "What is the definition of a higher-order function?"
+  }, {
+    "id": 8,
+    "questions": "Explain how prototypal inheritance works."
+  }, {
+    "id": 9,
+    "questions": "What's the difference between a variable that is: null, undefined or undeclared?"
+  }, {
+    "id": 10,
+    "questions": "What is a promise? Where and how would you use promise?"
+  }
+]
