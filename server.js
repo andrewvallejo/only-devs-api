d9baa4211f76e7edd722b387ad72ef6d2608ced1
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-07-26 13:51:41 -0400

    Update port to 3001 to run API with React

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 app.use(express.static('public'));
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 app.locals = { 
   title: 'onlyDevs API',
   questions
