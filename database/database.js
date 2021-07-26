0e3fe738d9a8b7c524b62a655a8d1f93f4236638
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-26 15:24:27 -0600

    Update password

--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool
 
 export const pool = new Pool({
   user: "postgres",
-  password: "password",
+  password: "devsonly",
   database: "questions_database",
   host: "localhost",
   port: 5432,
