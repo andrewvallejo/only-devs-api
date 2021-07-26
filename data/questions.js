e74aa4aa289cf5a2234645e068d00c737100ac64
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-25 18:46:08 -0600

    Fix const to be plural

--- data/questions.js
+++ data/questions.js
@@ -1,44 +1,44 @@
 export const questions = [
   {
     "id": 1,
-    "questions": "Explain what ARIA and screenreaders are, and how to make a website accessible.",
+    "question": "Explain what ARIA and screenreaders are, and how to make a website accessible.",
     "answers": [],
   }, {
     "id": 2,
-    "questions": "What is the virtual DOM in React?",
+    "question": "What is the virtual DOM in React?",
     "answers": [],
     
   }, {
     "id": 3,
-    "questions": "How are props different from state in React?",
+    "question": "How are props different from state in React?",
     "answers": [],
   }, {
     "id": 4,
-    "questions": "Explain 'hoisting'.",
+    "question": "Explain 'hoisting'.",
     "answers": [],
   }, {
     "id": 5,
-    "questions": "Describe event bubbling.",
+    "question": "Describe event bubbling.",
     "answers": [],
   }, {
     "id": 6,
-    "questions": "What are the differences between variables created using let, var or const?",
+    "question": "What are the differences between variables created using let, var or const?",
     "answers": [],
   }, {
     "id": 7,
-    "questions": "What is the definition of a higher-order function?",
+    "question": "What is the definition of a higher-order function?",
     "answers": [],
   }, {
     "id": 8,
-    "questions": "Explain how prototypal inheritance works.",
+    "question": "Explain how prototypal inheritance works.",
     "answers": [],
   }, {
     "id": 9,
-    "questions": "What's the difference between a variable that is: null, undefined or undeclared?",
+    "question": "What's the difference between a variable that is: null, undefined or undeclared?",
     "answers": [],
   }, {
     "id": 10,
-    "questions": "What is a promise? Where and how would you use promise?",
+    "question": "What is a promise? Where and how would you use promise?",
     "answers": [],
   }
 ]
