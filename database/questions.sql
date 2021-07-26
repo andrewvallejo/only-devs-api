e7298ed6fa1d1b41cd441cd5c4e4c2c7f448fed7
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-26 10:08:12 -0600

    Add sql commands for question and answer table

--- database/questions.sql
+++ database/questions.sql
@@ -1,6 +1,25 @@
 CREATE DATABASE questions_database;
 
-CREATE TABLE questions{
-  question_id SERIAL PRIMARY KEY,
-  description VARCHAR(500)
-}
+CREATE TABLE questions 
+(
+  question_id SERIAL PRIMARY KEY NOT NULL,
+  question TEXT NOT NULL,
+);
+
+
+
+CREATE TABLE answers 
+(
+  answer_id SERIAL PRIMARY KEY NOT NULL,
+  question_id integer NOT NULL references questions(question_id),
+  answer VARCHAR(500) NOT NULL,
+  rating integer NOT NULL,
+);
+
+-- RATING = 0 <-- ANSWER
+-- primary key?
+-- Serial
+-- What happens if we didn't have rating at all in our data,
+-- and wanted to add that feature late
+-- How to edit table (UPDATE)
+-- How to manipulate 
