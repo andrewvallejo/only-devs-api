0c3ab8df1a85100b43c464dafa688cb7f7b7daaf
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-26 19:13:16 -0600

    Refactor sql commands to be more to standard

--- database/questions.sql
+++ database/questions.sql
@@ -1,26 +1,15 @@
 CREATE DATABASE questions_database;
 
-CREATE TABLE questions 
-(
-  QUESTION_ID SERIAL PRIMARY KEY NOT NULL,
-  QUESTION TEXT NOT NULL,
-)
+CREATE TABLE questions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  question TEXT NOT NULL
+);
 
-INSERT INTO questions (question)
-VALUES ('HOW MUCH WOULD A WOODCHUCK CHUCK?...')
+CREATE TABLE answers (
+  id SERIAL PRIMARY KEY NOT NULL,
+  question_id integer NOT NULL references questions(id),
+  answer TEXT NOT NULL,
+  rating integer NOT NULL,
+  answer_time DATE NOT NUll
+);
 
-CREATE TABLE answers 
-(
-  ANSWER_ID SERIAL PRIMARY KEY NOT NULL,
-  QUESTION_ID integer NOT NULL references questions(QUESTION_ID),
-  ANSWER VARCHAR(500) NOT NULL,
-  RATING integer NOT NULL,
-)
-
--- RATING = 0 <-- ANSWER
--- primary key?
--- Serial
--- What happens if we didn't have rating at all in our data,
--- and wanted to add that feature late
--- How to edit table (UPDATE)
--- How to manipulate 
