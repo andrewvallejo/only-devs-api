ba6b65ee4a066cd7798790f28ba40a296b735b7c
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-26 19:14:40 -0600

    Refactor get call to work with postgres

--- server.js
+++ server.js
@@ -1,57 +1,52 @@
-import express from 'express';
-import cors from 'cors';
-import { questions } from './data/questions';
-import { pool } from './database/database';
+import express from "express";
+import cors from "cors";
+import { questions } from "./data/questions";
+import { pool } from "./database/database";
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
-app.set('port', process.env.PORT || 3001);
-app.locals = { 
-  title: 'onlyDevs API',
-  questions
-}
+app.use(express.static("public"));
+app.set("port", process.env.PORT || 3001);
+app.locals = {
+  title: "onlyDevs API",
+  questions,
+};
 
-app.get('/questions', (request, response) => {
-  pool.query('SELECT * FROM questions', (error, results) => {
-    if (error) {
-      throw error
-    } 
-    response.status(200).json(results)
-  })
-})
+app.get("/questions", async (request, response) => {
+  try {
+    await pool.query("SELECT * FROM questions", (error, results) => {
+      if (error) {
+        return console.error(`Query ${error.stack}`);
+      }
+      response.status(200).json(results.rows);
+    });
+  } 
+  catch (error) {
+    response.status(422).send("Sorry! The server is down!");
+  }
+});
 
 
-//   response.json({ questions })
-// })
 
 // app.get('/questions/:id', (request, response) => {
 //   const questions = app.locals.questions;
-//   const { id } = request.params 
+//   const { id } = request.params
 //   const queriedQuestion = questions.find(question => question.id === parseInt(id));
-  
+
 //   !queriedQuestion
 //     ? response.status(404).send('This question is not found!')
 //     : response.status(200).json(queriedQuestion)
 // })
 
-app.post('/questions', async (request, response) => { 
-  try
-  {
-  const { question } = request.body;
-  const newQuestion = await pool.query(
-    "INSERT INTO questions(question) VALUES ($1)",
-    [question]
-      ).then(response.status(200).json(question))
-} catch (error) {
-    console.error(error.message)
-  }
-})
+// Answers
 
-
-
-
-app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+app.listen(app.get("port"), () => {
+  console.log(
+    `${
+      app.locals.title
+    } is running on http://localhost:${app.get(
+      "port"
+    )}.`
+  );
 });
