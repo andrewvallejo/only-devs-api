bf5ac39b582ec8e3a822f0f60be25e572a536de9
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-26 19:21:34 -0600

    Merge branch 'feature/postgres' of github.com:andrewvallejo/only-devs-api into feature/postgres

--- server.js
+++ server.js
@@ -51,11 +51,5 @@ app.post("/questions", async (request, response) => {
 // Answers
 
 app.listen(app.get("port"), () => {
-  console.log(
-    `${
-      app.locals.title
-    } is running on http://localhost:${app.get(
-      "port"
-    )}.`
-  );
+  console.log(`${app.locals.title} is running on http://localhost:${app.get("port")}.`);
 });
