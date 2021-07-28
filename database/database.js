4bd9d84da383f05edd2c86a59e68f41bc6557353
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-07-27 19:33:11 -0600

    Remove password

--- database/database.js
+++ database/database.js
@@ -2,7 +2,6 @@ const Pool = require('pg').Pool
 
 export const pool = new Pool({
   user: "postgres",
-  password: "devsonly",
   database: "questions_database",
   host: "localhost",
   port: 5432,
