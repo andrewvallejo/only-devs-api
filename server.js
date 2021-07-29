6babc043f1d78586c7f0786354e6c7006a2f5084
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-29 14:27:57 -0600

    Remove comments

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const cors = require("cors");
 const {Pool} = require('pg')
 
+// const pool = require('./config')
 
 const pool = new Pool({
   connectionString: "postgres://afuxtawmyieyjo:13bb8d5ece25a9242c5ca5f55001027d17f329cfd3a91cecf780c5024e0239a4@ec2-3-237-55-96.compute-1.amazonaws.com:5432/d9aqkclakvsmeg",
@@ -17,53 +18,58 @@ app.use(express.static("public"));
 
 app.locals.title = "onlyDevs API"
 
-app.get('/questions', (request, response) => {
-     pool.query("SELECT * FROM questions", (error, results) => {  
+app.get("/questions", async (request, response) => {
+  pool.query("SELECT * FROM questions", (error, results) => {
+      if (error) {
+        return console.error(`Query ${error.stack}`);
+      }
       response.status(200).json(results.rows);
     });
+  
+    response.status(422).send("Sorry! The server is down!");
 });
 
-// app.post("/questions", async (request, response) => {
-//   try {
-//     const { question } = request.body;
-//     await pool.query("INSERT INTO questions(question) VALUES ($1)",
-//       [question]
-//     );
-//     return response.status(200);
-//   } 
-//   catch (error) {
-//     response.status(404).send(error.message); 
-//   }
-// });
+app.post("/questions", async (request, response) => {
+  try {
+    const { question } = request.body;
+    await pool.query("INSERT INTO questions(question) VALUES ($1)",
+      [question]
+    );
+    return response.status(200);
+  } 
+  catch (error) {
+    response.status(404).send(error.message); 
+  }
+});
 
-// app.get('/questions/:id', async (request, response) => {
-//   try {
-//     const { id } = request.params
-//     await pool.query(`SELECT * FROM ANSWERS WHERE question_id = ${id}`, (error, results) => {
-//       if (error) {
-//         return console.error(`Query ${error.stack}`);
-//       }
-//       response.status(200).json(results.rows);
-//     });
-//   }
-//   catch (error) {
-//     response.status(422).send("Sorry! The server is down!");
-//   }
-// });
+app.get('/questions/:id', async (request, response) => {
+  try {
+    const { id } = request.params
+    await pool.query(`SELECT * FROM ANSWERS WHERE question_id = ${id}`, (error, results) => {
+      if (error) {
+        return console.error(`Query ${error.stack}`);
+      }
+      response.status(200).json(results.rows);
+    });
+  }
+  catch (error) {
+    response.status(422).send("Sorry! The server is down!");
+  }
+});
 
-// app.post('/questions/answer', (request, response) => {
-//     const { question_id, answer } = request.body;
-//      pool.query('INSERT INTO answers(question_id, answer, rating) VALUES ($1, $2, $3)',
-//     [question_id, answer, 0], 
-//     (error, results) => {
+app.post('/questions/answer', (request, response) => {
+    const { question_id, answer } = request.body;
+     pool.query('INSERT INTO answers(question_id, answer, rating) VALUES ($1, $2, $3)',
+    [question_id, answer, 0], 
+    (error, results) => {
       
-//       if (error) {
-//         return console.error(error);
-//       }
-//       response.status(201).json("Successful post");
-//     });  
+      if (error) {
+        return console.error(error);
+      }
+      response.status(201).json("Successful post");
+    });  
   
-// })
+})
 
 
 app.listen(process.env.PORT || 3001, () => {
