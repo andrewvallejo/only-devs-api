cec5d823c8d6203acb5317f699dc1d902bc442d7
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-29 15:04:23 -0600

    Refactor app calls to work with heroku

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static("public"));
 
 app.locals.title = "onlyDevs API"
 
-app.get("/questions",  (request, response) => {
+app.get("/questions", (request, response) => {
   pool.query("SELECT * FROM questions", (error, results) => {
       if (error) {
         throw error
@@ -27,47 +27,31 @@ app.get("/questions",  (request, response) => {
     });
   });
 
-// app.post("/questions", async (request, response) => {
-//   try {
-//     const { question } = request.body;
-//     await pool.query("INSERT INTO questions(question) VALUES ($1)",
-//       [question]
-//     );
-//     return response.status(200);
-//   } 
-//   catch (error) {
-//     return response.status(404).send(error.message); 
-//   }
-// });
+app.post("/questions", (request, response) => {
+  const { question } = request.body;
+  pool.query("INSERT INTO questions(question) VALUES ($1)",
+  [question]
+  );
+  return response.status(200);
+});
 
-// app.get('/questions/:id', async (request, response) => {
-//   try {
-//     const { id } = request.params
-//     await pool.query(`SELECT * FROM ANSWERS WHERE question_id = ${id}`, (error, results) => {
-//       if (error) {
-//         return console.error(`Query ${error.stack}`);
-//       }
-//       return response.status(200).json(results.rows);
-//     });
-//   }
-//   catch (error) {
-//     return response.status(422).send("Sorry! The server is down!");
-//   }
-// });
+app.get('/questions/:id', (request, response) => {
+  const { id } = request.params
+  pool.query(`SELECT * FROM ANSWERS WHERE question_id = ${id}`, (error, results) => {
+    if (error) {
+      throw error
+    }
+  return response.status(200).json(results.rows);
+  });
+});
 
-// app.post('/questions/answer', (request, response) => {
-//     const { question_id, answer } = request.body;
-//      pool.query('INSERT INTO answers(question_id, answer, rating) VALUES ($1, $2, $3)',
-//     [question_id, answer, 0], 
-//     (error, results) => {
-      
-//       if (error) {
-//         return console.error(error);
-//       }
-//       return response.status(201).json("Successful post");
-//     });  
-  
-// })
+app.post('/questions/answer', (request, response) => {
+  const { question_id, answer } = request.body;
+  pool.query('INSERT INTO answers(question_id, answer, rating) VALUES ($1, $2, $3)',
+  [question_id, answer, 0]
+  );
+  return response.status(201).json("Successful post"); 
+})
 
 
 app.listen(process.env.PORT || 3001, () => {
