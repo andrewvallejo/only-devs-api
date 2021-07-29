9c8fa3c9903903939b2ded8df11b310e2e747ff8
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-29 13:39:39 -0600

    Remove SSL

--- config.js
+++ config.js
@@ -6,11 +6,7 @@ const isProduction = process.env.NODE_ENV === 'production'
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 const pool = new Pool({
-  connectionString: isProduction ? "postgres://afuxtawmyieyjo:13bb8d5ece25a9242c5ca5f55001027d17f329cfd3a91cecf780c5024e0239a4@ec2-3-237-55-96.compute-1.amazonaws.com:5432/d9aqkclakvsmeg" : connectionString,
-  ssl: {
-    sslmode: 'require',
-    rejectUnauthorized: false,
-  }
+  connectionString: isProduction ? "postgres://afuxtawmyieyjo:13bb8d5ece25a9242c5ca5f55001027d17f329cfd3a91cecf780c5024e0239a4@ec2-3-237-55-96.compute-1.amazonaws.com:5432/d9aqkclakvsmeg" : connectionString
 })
 
 
