e0db77b4226f1186d789815aa647cfeeac0a3173
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-28 23:08:07 -0600

    Remove comments

--- server.js
+++ server.js
@@ -25,49 +25,49 @@ app.get("/questions", async (request, response) => {
   }
 });
 
-// app.post("/questions", async (request, response) => {
-//   try {
-//     const { question } = request.body;
-//     await pool.query("INSERT INTO questions(question) VALUES ($1)",
-//       [question]
-//     );
-//     return response.status(200);
-//   } 
-//   catch (error) {
-//     response.status(404).send(error.message); 
-//   }
-// });
+app.post("/questions", async (request, response) => {
+  try {
+    const { question } = request.body;
+    await pool.query("INSERT INTO questions(question) VALUES ($1)",
+      [question]
+    );
+    return response.status(200);
+  } 
+  catch (error) {
+    response.status(404).send(error.message); 
+  }
+});
 
-// app.get('/questions/:id', async (request, response) => {
-//   try {
-//     const { id } = request.params
-//     await pool.query(`SELECT * FROM ANSWERS WHERE question_id = ${id}`, (error, results) => {
-//       if (error) {
-//         return console.error(`Query ${error.stack}`);
-//       }
-//       response.status(200).json(results.rows);
-//     });
-//   }
-//   catch (error) {
-//     response.status(422).send("Sorry! The server is down!");
-//   }
-// });
+app.get('/questions/:id', async (request, response) => {
+  try {
+    const { id } = request.params
+    await pool.query(`SELECT * FROM ANSWERS WHERE question_id = ${id}`, (error, results) => {
+      if (error) {
+        return console.error(`Query ${error.stack}`);
+      }
+      response.status(200).json(results.rows);
+    });
+  }
+  catch (error) {
+    response.status(422).send("Sorry! The server is down!");
+  }
+});
 
-// app.post('/questions/answer', (request, response) => {
-//     const { question_id, answer } = request.body;
-//      pool.query('INSERT INTO answers(question_id, answer, rating) VALUES ($1, $2, $3)',
-//     [question_id, answer, 0], 
-//     (error, results) => {
+app.post('/questions/answer', (request, response) => {
+    const { question_id, answer } = request.body;
+     pool.query('INSERT INTO answers(question_id, answer, rating) VALUES ($1, $2, $3)',
+    [question_id, answer, 0], 
+    (error, results) => {
       
-//       if (error) {
-//         return console.error(error);
-//       }
-//       response.status(201).json("Successful post");
-//     });  
+      if (error) {
+        return console.error(error);
+      }
+      response.status(201).json("Successful post");
+    });  
   
-// })
+})
 
 
-app.listen(process.env.PORT || 3001, () => {
+app.listen(process.env.PORT || 3001, '0.0.0.0', () => {
   console.log("Server is running.");
 });
